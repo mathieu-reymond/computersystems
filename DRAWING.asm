5f4cfa846fde5f71b3cf20cf9c177bd9c2d14d60
mathieu-reymond <mathieu.reymond@gmail.com>
2013-11-21 17:54:13 +0100

    final bullshit commit
    bullshit RIP

--- DRAWING.asm
+++ DRAWING.asm
@@ -1,126 +1,307 @@
-;==================
-; Game drawings
-;==================
-.MODEL large
+; ==============================================================================
+; Example for buffered drawing in mode 13h.
+; Example showing palette manipulation via port IO.
+; Example for a very generic game-loop.
+; ==============================================================================
+.MODEL large	; multiple data segments and multiple code segments
+.STACK 2048  	; stack
 
-;----INCLUDES------
+; --- INCLUDES -----------------------------------------------------------------
 
+include VIDEO.INC
+include RAND.INC
+include KEYB.INC
 
+; --- MACROS AND CONSTANTS -----------------------------------------------------
 
-;-----MACRO'S and CONSTANTS------
+; Other constants	
+SCREENW		equ 320
+SCREENH		equ 200
 
-SCREEN_X equ 320
-SCREEN_Y equ 200
+; --- DATA SEGMENT -------------------------------------------------------------
+.DATA        ; data segment, variables
+oldVideoMode	db ?
 
-;-----SCREEN BUFFER-------
-.FARDATA?
+hardOffset	dw 0 ; test variable
 
-palette db 0,0,0,63,63,63
-screenBuffer db SCREEN_X * SCREEN_Y dup(?) ;pixels of screen
+; --- SCREEN BUFFER ------------------------------------------------------------
+.FARDATA?	; segment that contains the screenBuffer for mode 13h drawing
+palette			db 768 dup(0)
+screenBuffer	db 64000 dup(?)	; the 64000 bytes for the screen
 
-;--------CODE SEGMENT-------
-.CODE
+; --- CODE SEGMENT -------------------------------------------------------------
+.CODE        ; code segment
+main PROC NEAR
+	mov	ax, @data	; get data segment address
+	mov	ds, ax		; set DS to data segment
 
-;get pixel offset for given board-coord
-;AX the given coord
-;@return AX the offset
-offsetForCoord PROC NEAR
-	push bx
-	push dx
-
-	;offset = y*SCREEN_X*(SCREEN_Y/BOARD_Y) + x*(SCREEN_X/BOARD_X)
-	mov bx, ax
-	mov ah, 0 ;AX = y
-	mov dx, SCREEN_X
-	mul dx ;AX = SCREEN_X*y
-	mov dx, 10 ;(SCREEN_Y/BOARD_Y)
-	mul dx ;AX = y*SCREEN_X*(SCREEN_Y/BOARD_Y)
+	; Initialize random number generator
+	call	randInit
 	
-	push ax ;save current offset
+	; Install our own keyboard handler
+	call	installKeyboardHandler
+
+	; fade to black
+	call	fadeToBlack
 	
-	mov al, bh
-	mov ah, 0 ;AX = x
-	mov dx, 10 ;(SCREEN_X/BOARD_X)
-	mul dx ;AX = x*(SCREEN_X/BOARD_X)
+	; clear video buffer
+	call	clearScreenBuffer
+
+	; draw the screen buffer
+	call 	updateScreen
 	
-	pop bx ;restore offset in bx
-	add ax, bx ;AX = y*SCREEN_X*(SCREEN_Y/BOARD_Y) + x*(SCREEN_X/BOARD_X)
+	; set mode 13h
+	mov		ax, 13h
+	push	ax
+	call	setVideoMode
+	mov		[oldVideoMode], al
 	
-	pop dx
-	pop bx
+@main_loop:	
+	;call	updateWorld	; this would contain the game code (like AI)
+
+	call	renderWorld	; draws the world
+	
+	call	handleInput	; handle user input
+	cmp		al, 0
+	jz		@main_loop
+	
+	; Restore original keyboard handler
+	call	uninstallKeyboardHandler
+
+	; Restore original video mode
+	mov		al, [oldVideoMode]
+	xor		ah, ah
+	push	ax
+	call	setVideoMode
+	
+	; Exit to DOS
+	mov		ax, 4c00h	; exit to DOS function, return code 00h
+	int		21h			; call DOS
+main ENDP
+
+; Fades the active colors to black
+fadeToBlack PROC NEAR
+	push	ax
+
+	mov	ax, seg palette
+	push	ax
+	mov	ax, offset palette
+	push	ax
+	call	paletteInitFade
+@@:
+	waitVBlank
+	call	paletteNextFade
+	test	ax, ax
+	jnz	@B
+
+	pop	ax
 	ret 0
-offsetForCoord ENDP
+fadeToBlack ENDP
 
-;draw a snake-head at given coord in AX
-drawHead PROC FAR
-	push bx
-	push dx
-	push di
-	push si
-	push es
+; Clears the screen buffer to color 0
+clearScreenBuffer PROC NEAR
+	push	ax
+	push	cx
+	push	di
+	push	es
 	
-	;mov ax, seg screenBuffer ;temp mov to ax
-	;add ax, offset screenBuffer
-	;mov es, ax ;screenBuffer in es (es points to first pixel of screen)
-	mov bx, 0a000h
-	mov es, bx
+	cld
+	mov		ax, seg screenBuffer
+	mov		es, ax
+	mov		di, offset screenBuffer
+	mov		cx, 64000 / 2
+	xor		ax, ax
+	rep		stosw
 	
-	call offsetForCoord ;AX = pixel offset
-	mov di, ax
-	mov dx, 1 ;color from palette
+	pop	es
+	pop	di
+	pop	cx
+	pop	ax
+	ret	0
+clearScreenBuffer ENDP
+
+; Updates the screen (copies contents from screenBuffer to screen)
+updateScreen PROC NEAR
+	push	ax
+	push	cx
+	push	dx
+	push	si
+	push	di
+	push	ds
+	push	es
 	
-	;temp : make a filled square
-	mov bx, 0
-@yLoop:
-	mov si, 0
-@xLoop:
-	mov es:[di], dx ;set color at given offset
+	; setup source and dest segments
+	mov		ax, seg screenBuffer
+	mov		ds, ax
+	mov		si, offset screenBuffer
+	mov		ax, 0a000h	; video memory
+	mov		es, ax
+	xor		di, di	; start at pixel 0
+	
+	cld
+	mov		cx, 64000 / 2
+	waitVBlank	; wait for a VB (modifies AX and DX)
+	rep		movsw	; blit to screen	
+	
+	pop		es
+	pop		ds
+	pop		di
+	pop		si
+	pop		dx
+	pop		cx
+	pop		ax
+	ret		0
+updateScreen ENDP
+
+renderWorld PROC NEAR
+	push	ax
+	
+	call	clearScreenBuffer
+
+	; draw a random rectangle
+	call	rand
+	and		ax, 0ffh
+	push	ax
+	call	rand
+	and		ax, 07fh
+	push	ax
+	call 	drawRect
+
+	; draw the screen buffer
+	call 	updateScreen
+	
+	pop	ax
+	ret	0
+renderWorld ENDP
+
+; Reads keyboard buffer and acts (returns non-zero if loop should end, 0 otherwise)
+handleInput PROC NEAR
+	push	es
+
+	mov	ax, seg __keysActive
+	mov	es, ax
+
+	xor	ah, ah
+	mov	al, es:[__keysActive]
+	cmp	al, 0
+	jz	@done		; no key pressed
+
+	; handle keys
+	mov	al, es:[__keyboardState][SCANCODE_UP]	; test UP key
+	cmp	al, 0
+	jz @F	; jump next
+	; call some function to handle this key
+	mov	ax, SCREENW
+	sub [hardOffset], ax
+	sub [hardOffset], ax
+@@:
+	mov	al, es:[__keyboardState][SCANCODE_DOWN]	; test DOWN key
+	cmp	al, 0
+	jz @F	; jump next
+	; call some function to handle this key
+	mov	ax, SCREENW
+	add [hardOffset], ax
+	add [hardOffset], ax
+@@:
+	mov	al, es:[__keyboardState][SCANCODE_LEFT]	; test LEFT key
+	cmp	al, 0
+	jz @F	; jump next
+	; call some function to handle this key
+	dec [hardOffset]
+	dec [hardOffset]
+@@:
+	mov	al, es:[__keyboardState][SCANCODE_RIGHT]	; test RIGHT key
+	cmp	al, 0
+	jz @F	; jump next
+	; call some function to handle this key
+	inc [hardOffset]
+	inc [hardOffset]
+@@:
+	
+	; finally, let's put the ESC key status as return value in AX
+	mov	al, es:[__keyboardState][SCANCODE_ESC]	; test ESC
+
+@done:
+	pop	es
+	ret 0
+handleInput ENDP
+
+; Draw a rectangle at the center of the screen buffer.
+; W, H passed on stack.
+drawRect PROC NEAR
+	push	bp
+	mov	bp, sp
+	
+	push	ax
+	push	bx
+	push	cx
+	push	dx
+	push	di
+	push	es
+	
+	; set segment
+	mov	ax, seg screenBuffer
+	mov	es, ax
+	
+	; Calculate posX
+	mov	ax, [bp + 4][2]
+	neg	ax
+	add	ax, SCREENW
+	shr	ax, 1
+	mov	bx, ax		; posX is in BX now
+	
+	; Calculate posY
+	mov	ax, [bp + 4][0]
+	neg	ax
+	add	ax, SCREENH
+	shr	ax, 1		; and posY is in AX
+	
+	; Calculate offset of top-left corner
+	mov	dx, SCREENW
+	mul	dx		; AX = posY * SCREENW
+	add	ax, bx		; AX now contains start offset of rectangle
+	add	ax, offset screenBuffer
+	add	ax, [hardOffset]
+	
+	; Draw upper horizontal line
+	mov	di, ax
+	mov	cx, [bp + 4][2]	; rect W
+	mov	al, 15	; color
+	cld
+	rep	stosb	; draw	
+	
+	; Draw right vertical line	
+	mov		cx, [bp + 4][0]	; rect H
+	dec	di
+@@:
+	mov	es:[di], al	; set pixel
+	add	di, SCREENW	; jump to next pixel (on next line)
+	loop	@B
+	
+	; Draw bottom horizontal line
+	mov	cx, [bp+4][2]	; rect W
+	std	; count backwards
+	rep	stosb	; draw
+	
+	; Draw left vertical line
+	mov	cx, [bp + 4][0]	; rect H
 	inc di
-	inc si
-	cmp si, 10 ;;(SCREEN_X/BOARD_X)
-	jnz @xLoop
-	
-	mov ax, SCREEN_X
-	sub ax, 10
-	add di, ax
-	inc bx
-	cmp bx, 10 ;(SCREEN_Y/BOARD_Y)
-	jnz @yLoop
-	
-	pop es
-	pop si
-	pop di
-	pop dx
-	pop bx
-	ret 0
-drawHead ENDP
+@@:
+	mov	es:[di], al	; set pixel
+	sub	di, SCREENW	; jump to next pixel (on next line)
+	loop	@B
 
-initDrawing PROC FAR
-	push ax
-	push bx
-	push cx
-	push ds
-	push es
-	;set resolution to default (320x200)
-	mov ax, 13h
-	int 10h
-	
-	;set palette (0= black, 1=white)
-	mov ax, @data
-	mov ds, ax
-	mov es, ax
-	mov dx, offset palette
-	xor bx, bx
-	mov cx, 2
-	mov ax, 1012h
-	int 10h
-	
-	pop es
-	pop ds
-	pop cx
-	pop bx
-	pop ax
-	
-	ret 0
-initDrawing ENDP
-END
+	; We are done
+	pop	es
+	pop	di
+	pop	dx
+	pop	cx
+	pop	bx
+	pop	ax
+	; return
+	pop	bp
+	ret	4
+drawRect ENDP
+
+; _------------------------------- END OF CODE ---------------------------------
+END main
---
+++ DRAWING1.asm
@@ -0,0 +1,126 @@
+;==================
+; Game drawings
+;==================
+.MODEL large
+
+;----INCLUDES------
+
+
+
+;-----MACRO'S and CONSTANTS------
+
+SCREEN_X equ 320
+SCREEN_Y equ 200
+
+;-----SCREEN BUFFER-------
+.FARDATA?
+
+palette db 0,0,0,63,63,63
+screenBuffer db SCREEN_X * SCREEN_Y dup(?) ;pixels of screen
+
+;--------CODE SEGMENT-------
+.CODE
+
+;get pixel offset for given board-coord
+;AX the given coord
+;@return AX the offset
+offsetForCoord PROC NEAR
+	push bx
+	push dx
+
+	;offset = y*SCREEN_X*(SCREEN_Y/BOARD_Y) + x*(SCREEN_X/BOARD_X)
+	mov bx, ax
+	mov ah, 0 ;AX = y
+	mov dx, SCREEN_X
+	mul dx ;AX = SCREEN_X*y
+	mov dx, 10 ;(SCREEN_Y/BOARD_Y)
+	mul dx ;AX = y*SCREEN_X*(SCREEN_Y/BOARD_Y)
+	
+	push ax ;save current offset
+	
+	mov al, bh
+	mov ah, 0 ;AX = x
+	mov dx, 10 ;(SCREEN_X/BOARD_X)
+	mul dx ;AX = x*(SCREEN_X/BOARD_X)
+	
+	pop bx ;restore offset in bx
+	add ax, bx ;AX = y*SCREEN_X*(SCREEN_Y/BOARD_Y) + x*(SCREEN_X/BOARD_X)
+	
+	pop dx
+	pop bx
+	ret 0
+offsetForCoord ENDP
+
+;draw a snake-head at given coord in AX
+drawHead PROC FAR
+	push bx
+	push dx
+	push di
+	push si
+	push es
+	
+	;mov ax, seg screenBuffer ;temp mov to ax
+	;add ax, offset screenBuffer
+	;mov es, ax ;screenBuffer in es (es points to first pixel of screen)
+	mov bx, 0a000h
+	mov es, bx
+	
+	call offsetForCoord ;AX = pixel offset
+	mov di, ax
+	mov dx, 1 ;color from palette
+	
+	;temp : make a filled square
+	mov bx, 0
+@yLoop:
+	mov si, 0
+@xLoop:
+	mov es:[di], dx ;set color at given offset
+	inc di
+	inc si
+	cmp si, 10 ;;(SCREEN_X/BOARD_X)
+	jnz @xLoop
+	
+	mov ax, SCREEN_X
+	sub ax, 10
+	add di, ax
+	inc bx
+	cmp bx, 10 ;(SCREEN_Y/BOARD_Y)
+	jnz @yLoop
+	
+	pop es
+	pop si
+	pop di
+	pop dx
+	pop bx
+	ret 0
+drawHead ENDP
+
+initDrawing PROC FAR
+	push ax
+	push bx
+	push cx
+	push ds
+	push es
+	;set resolution to default (320x200)
+	mov ax, 13h
+	int 10h
+	
+	;set palette (0= black, 1=white)
+	mov ax, @data
+	mov ds, ax
+	mov es, ax
+	mov dx, offset palette
+	xor bx, bx
+	mov cx, 2
+	mov ax, 1012h
+	int 10h
+	
+	pop es
+	pop ds
+	pop cx
+	pop bx
+	pop ax
+	
+	ret 0
+initDrawing ENDP
+END
